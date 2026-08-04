39547df4e889929c80a8d38b60b8452c95429513
Jamie12341324 <peacfulmarinecat@gmail.com>
2026-08-04 17:09:53 +0100

    Add AI text list page without some functions

--- update_AI/urls.py
+++ update_AI/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('AI_writing/', views.AI_writing, name='AI_writing'),
     path('AI_text_list/<int:ai_id>', views.AI_text_list, name='AI_text_list'),
     path('update_AI/<int:ai_id>', views.update_AI, name='update_AI'),
+    path('update_AI2/<int:ai_id>/<int:text_id>', views.update_AI2, name='update_AI2'),
+    path('text_rename/<int:ai_id>/<int:text_id>', views.text_rename, name='text_rename'),
+    path('text_delete/<int:ai_id>/<int:text_id>', views.text_delete, name='text_delete'),
 ]
--- update_AI/views.py
+++ update_AI/views.py
@@ -1,29 +1,28 @@
 from django.shortcuts import render, redirect
 from django.db.models import Q
 from .models import training_text,AI
+from django.db.models import Max
 # Create your views here.
 def AI_writing(request,ai_id):
     template='AI_writing.html'
     return render(request,template)
 def update_AI(request,ai_id):
-    template='update_AI.html'
-    #return render(request,template,context)
+    ai = AI.objects.get(id=ai_id)
     if request.method == "POST":
             if request.POST["train_AI"] == '':
                 return redirect("update_AI", ai_id=ai.id)
-            ai_training_records = training_text.objects.filter(Q(user_id=request.user.id)).values()
-            if not ai_training_records.exists():
-                ai = AI.objects.get(id=ai_id)
-                count = training_text.objects.filter(user=request.user,ai=ai.id).count()+1
-                text=training_text()
-                text.user_id=request.user.id
-                text.text_saved=request.POST["train_AI"]
-                text.name2=count
-                text.ai=ai
-                text.save()
-                return redirect("AI_text_list", ai_id=ai.id)
+            ai = AI.objects.get(id=ai_id)
+            max_num = training_text.objects.filter(user=request.user,ai=ai.id).aggregate(Max("name2"))["name2__max"]
+            text=training_text()
+            text.user_id=request.user.id
+            text.text_saved=request.POST["train_AI"]
+            if max_num==None:
+                text.name2=1
             else:
-                return redirect("update_AI", ai_id=ai.id)
+                text.name2=max_num+1
+            text.ai=ai
+            text.save()
+            return redirect("AI_text_list", ai_id=ai.id)
     else:
         training_texts=training_text.objects.all().values()
         context={"training_texts":training_texts,
@@ -34,6 +33,31 @@ def update_AI(request,ai_id):
             "update_AI.html",
             context
         )
+def update_AI2(request,ai_id,text_id):
+     context={}
+     return render(
+                 request,
+                 "update_AI.html",
+                 context
+             )
+def text_rename(request,ai_id,text_id):
+     context={}
+     return render(
+                 request,
+                 "update_AI.html",
+                 context
+             )
+def text_delete(request,ai_id,text_id):
+     context={}
+     return render(
+                 request,
+                 "update_AI.html",
+                 context
+             )
 def AI_text_list(request,ai_id):
     template='AI_text_list.html'
-    return render(request,template)
+    Text_results=training_text.objects.filter(user=request.user,ai=ai_id)
+    ai = AI.objects.get(id=ai_id)
+    context={'Text_results':Text_results,
+             "ai":ai}
+    return render(request,template,context)
