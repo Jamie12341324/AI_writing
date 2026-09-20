61b8b633e93375e9e872f07a7668de6409a8f50c
Jamie12341324 <peacfulmarinecat@gmail.com>
2026-09-20 20:47:06 +0100

    Add collect static

---
+++ staticfiles/js/script2.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function(){
+let form2=document.getElementById("input_form");
+    form2.addEventListener("submit", function(event){
+        const button = event.submitter;
+        let text="";
+        if (button.id=="talk_AI_button"){
+            text=document.getElementById("talk_AI").value;
+        }
+        else if (button.id=="talk_AI_button2"){
+            text=document.getElementById("response_AI").value;
+        }
+        else if (button.id=="talk_AI_button2a"){
+            text=document.getElementById("response_AI1").value;
+        }
+        else if (button.id=="talk_AI_button2b"){
+            text=document.getElementById("response_AI2").value;
+        }
+        else if (button.id=="talk_AI_button2c"){
+            text=document.getElementById("response_AI3").value;
+        }
+        if (text.length<2){
+            alert("Starting texts or response texts used as a start must have a value to be used to make a response text.");
+            event.preventDefault();
+        }
+    });
+});
